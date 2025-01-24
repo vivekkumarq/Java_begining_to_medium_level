fa48ce73725732b60d0eda80c8113a88abdbebfd
vivek kumar <51886550+vivekkumarq@users.noreply.github.com>
2025-01-25 00:46:23 +0530

    Arrays

---
+++ LearnCollection.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class LearnCollection {
+    public static void main(String[] args) {
+        ArrayList<String> studentsName = new ArrayList<>();
+        studentsName.add("Raju");
+        studentsName.add("Raman");
+        //So we can add as many as names in studentsName as its dynamic
+        System.out.println("Here is list of students name" + studentsName);
+
+    }
+}
