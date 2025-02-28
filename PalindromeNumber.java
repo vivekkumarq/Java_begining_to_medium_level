7c27d9567f7bf189d1f7296173686ff081da2f45
vivek kumar <51886550+vivekkumarq@users.noreply.github.com>
2025-03-01 02:33:32 +0530

    PalindromeProblem

---
+++ PalindromeNumber.java
@@ -0,0 +1,18 @@
+public class PalindromeNumber{
+    public static boolean isPalindrome(int x){
+    int reverse = 0;
+    int original = x;
+    while(x>0){
+        int digit = x%10;
+        reverse = reverse*10 +digit;
+        x=x/10;
+    }
+    return original == reverse;
+}
+public static void main(String[] args) {
+    int x1=121;
+    System.out.println("is this palindrome " + isPalindrome(x1));
+    int x2 = 123;
+    System.out.println("is this palindrome " + isPalindrome(x2));
+}
+}
