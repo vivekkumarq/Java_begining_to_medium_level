9eb3263c48674214eb3a7cfecb64ad8359fea1d7
vivek kumar <51886550+vivekkumarq@users.noreply.github.com>
2025-03-11 00:28:16 +0530

    removeDuplicates

---
+++ RemoveDuplicates.java
@@ -0,0 +1,28 @@
+public class RemoveDuplicates {
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int k = 1; 
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2};
+        int k = removeDuplicates(nums);
+
+        System.out.println("Number of unique elements: " + k);
+        System.out.print("Updated array: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+    }
+}
