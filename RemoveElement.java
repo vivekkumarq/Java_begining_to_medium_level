a1a674b39488e7147e1d7bfbada1d1e83a8dfc32
vivek kumar <51886550+vivekkumarq@users.noreply.github.com>
2025-03-12 00:02:30 +0530

    RemoveElement

---
+++ RemoveElement.java
@@ -0,0 +1,25 @@
+public class RemoveElement{
+    public static int removeElement(int[] nums,int val)
+    {
+        int counter=0;
+        for(int i=0;i<nums.length;i++){
+if(nums[i] != val)
+{
+    nums[counter]=nums[i];
+    counter++;
+}
+        }
+        return counter;
+    }
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3};
+int val = 3;
+int counter = removeElement(nums, val);
+System.out.println("Number of elements not equal to " + val + ": " + counter);
+        System.out.print("Updated array: ");
+        for (int i = 0; i < counter; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+    }
+}
