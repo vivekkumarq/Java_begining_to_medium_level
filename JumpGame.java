81beeb9d92185fcef7e7e49242330336c3f01a76
vivek kumar <51886550+vivekkumarq@users.noreply.github.com>
2025-03-21 01:11:58 +0530

    JumpGame

---
+++ JumpGame.java
@@ -0,0 +1,21 @@
+public class JumpGame {
+    public static boolean canJump(int[] nums) {
+        int reachable = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > reachable) return false; 
+            reachable = Math.max(reachable, i + nums[i]);
+            if (reachable >= nums.length - 1) return true;
+        }
+
+        return true;
+    }
+
+ 
+    public static void main(String[] args) {
+
+        System.out.println(canJump(new int[]{2,3,1,1,4})); 
+        System.out.println(canJump(new int[]{3,2,1,0,4})); 
+    }
+    
+}
