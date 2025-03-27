fe0f8c72f965b6d1e423497ccded38532047cbc5
vivek kumar <51886550+vivekkumarq@users.noreply.github.com>
2025-03-28 01:42:25 +0530

    PascalTriangle

---
+++ PascalTriangle.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangle {
+     public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        if (numRows <= 0) {
+            return triangle;
+        }
+
+        triangle.add(new ArrayList<>());
+        triangle.get(0).add(1);
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            List<Integer> prevRow = triangle.get(i - 1);
+            row.add(1);
+
+            for (int j = 1; j < i; j++) {
+                row.add(prevRow.get(j - 1) + prevRow.get(j));
+            }
+
+       
+            row.add(1);
+
+            triangle.add(row);
+        }
+
+        return triangle;
+    }
+
+    public static void main(String[] args) {
+      
+
+        int numRows = 5;
+        List<List<Integer>> result = generate(numRows);
+        System.out.println(result);
+
+        int numRows2 = 1;
+        System.out.println(generate(numRows2));
+    }
+    
+}
