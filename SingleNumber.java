0f8638178f6efa9d31c9b0b2fdf82a3f17e6be87
vivek kumar <51886550+vivekkumarq@users.noreply.github.com>
2025-03-29 23:50:54 +0530

    SingleNumber

---
+++ SingleNumber.java
@@ -0,0 +1,23 @@
+public class SingleNumber {
+    public static int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result= result ^ num; //XOR operations is used
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        
+
+        int[] nums1 = {2, 2, 1};
+        System.out.println(singleNumber(nums1)); 
+
+        int[] nums2 = {4, 1, 2, 1, 2};
+        System.out.println(singleNumber(nums2)); 
+
+        int[] nums3 = {1};
+        System.out.println(singleNumber(nums3)); 
+    }
+    
+}
