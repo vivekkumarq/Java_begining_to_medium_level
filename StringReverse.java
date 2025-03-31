df834b39bfe6630ad55b765d1e7b80abd416ae02
vivek kumar <51886550+vivekkumarq@users.noreply.github.com>
2025-03-31 23:47:55 +0530

    ReverseStrings

---
+++ StringReverse.java
@@ -0,0 +1,28 @@
+public class StringReverse {
+    public static void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+        
+        while (left < right) {
+          
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+
+        char[] s1 = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(s1);
+        System.out.println(s1); 
+
+        char[] s2 = {'H', 'a', 'n', 'n', 'a', 'h'};
+        reverseString(s2);
+        System.out.println(s2);
+    }
+    
+}
