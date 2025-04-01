ad7a778916b73b89981c0a2f420cbc18bff3347b
vivek kumar <51886550+vivekkumarq@users.noreply.github.com>
2025-04-02 01:34:28 +0530

    mergeAlternately

---
+++ MergeAlternately.java
@@ -0,0 +1,28 @@
+public class MergeAlternately {
+    public static String mergeAlternately(String word1, String word2) {
+        StringBuilder result = new StringBuilder();
+        int i = 0, j = 0;
+
+        while (i < word1.length() && j < word2.length()) {
+            result.append(word1.charAt(i++));
+            result.append(word2.charAt(j++));
+        }
+     
+        while (i < word1.length()) {
+            result.append(word1.charAt(i++));
+        }
+        while (j < word2.length()) {
+            result.append(word2.charAt(j++));
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        
+        System.out.println(mergeAlternately("abc", "pqr"));  
+        System.out.println(mergeAlternately("ab", "pqrs"));  
+        System.out.println(mergeAlternately("abcd", "pq"));  
+    }
+    
+}
