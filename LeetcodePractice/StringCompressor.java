44fea3620e7ce299a3ee172602a1c10bc1b183a8
vivek kumar <51886550+vivekkumarq@users.noreply.github.com>
2025-04-10 02:29:48 +0530

    StringCompressor

---
+++ LeetcodePractice/StringCompressor.java
@@ -0,0 +1,43 @@
+package LeetcodePractice;
+public class StringCompressor {
+    public int compress(char[] chars) {
+        int index = 0;  
+        int i = 0;    
+
+        while (i < chars.length) {
+            char currentChar = chars[i];
+            int count = 0;
+
+            
+            while (i < chars.length && chars[i] == currentChar) {
+                i++;
+                count++;
+            }
+
+           
+            chars[index++] = currentChar;
+
+            
+            if (count > 1) {
+                for (char c : Integer.toString(count).toCharArray()) {
+                    chars[index++] = c;
+                }
+            }
+        }
+
+        return index; 
+    }
+
+    public static void main(String[] args) {
+        StringCompressor compressor = new StringCompressor();
+
+        char[] chars1 = {'a','a','b','b','c','c','c'};
+        System.out.println(compressor.compress(chars1)); 
+
+        char[] chars2 = {'a'};
+        System.out.println(compressor.compress(chars2)); 
+
+        char[] chars3 = {'a','b','b','b','b','b','b','b','b','b','b','b','b'};
+        System.out.println(compressor.compress(chars3)); 
+    }
+}
