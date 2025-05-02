6183f13c082ba36e8e0fbf00668cad21316f2369
vivek kumar <51886550+vivekkumarq@users.noreply.github.com>
2025-05-02 20:33:22 +0530

    ReverseLinked

---
+++ LeetcodePractice/ReverseLinked.java
@@ -0,0 +1,55 @@
+package LeetcodePractice;
+public class ReverseLinked {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) { this.val = val; }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next; // Save next
+            curr.next = prev;          // Reverse link
+            prev = curr;               // Move prev forward
+            curr = next;               // Move curr forward
+        }
+        return prev; // New head
+    }
+
+   
+    public static ListNode createList(int[] values) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        for (int val : values) {
+            current.next = new ListNode(val);
+            current = current.next;
+        }
+        return dummy.next;
+    }
+
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val);
+            if (head.next != null) System.out.print(" -> ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        int[] input = {1, 2, 3, 4, 5};  
+        ListNode head = createList(input);
+
+        System.out.print("Original List: ");
+        printList(head);
+
+        head = reverseList(head);
+
+        System.out.print("Reversed List: ");
+        printList(head);
+    }
+}
