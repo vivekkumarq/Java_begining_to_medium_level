0143b0ec4fcb36a69e734f535459931aef5054f3
vivek kumar <51886550+vivekkumarq@users.noreply.github.com>
2025-06-02 21:29:39 +0530

    TilingDominoTromino

---
+++ LeetcodePractice/TilingDominoTromino.java
@@ -0,0 +1,32 @@
+package LeetcodePractice;
+
+public class TilingDominoTromino {
+    public int numTilings(int n) {
+        int MOD = 1_000_000_007;
+
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        if (n == 3) return 5;
+
+        long[] dp = new long[n + 1];
+        dp[0] = 1;  
+        dp[1] = 1; 
+        dp[2] = 2;  
+        dp[3] = 5; 
+
+        for (int i = 4; i <= n; i++) {
+            dp[i] = (2 * dp[i - 1] % MOD + dp[i - 3] % MOD) % MOD;
+        }
+
+        return (int) dp[n];
+    }
+
+    public static void main(String[] args) {
+        TilingDominoTromino solver = new TilingDominoTromino();
+
+        System.out.println(solver.numTilings(3)); 
+        System.out.println(solver.numTilings(1)); 
+        System.out.println(solver.numTilings(4)); 
+    }
+}
+
