00c714110b78e8aee8c58371bca3f8d81917beaf
vivek kumar <51886550+vivekkumarq@users.noreply.github.com>
2025-06-22 19:49:23 +0530

    AnagramChecker

---
+++ LeetcodePractice/AnagramChecker.java
@@ -0,0 +1,28 @@
+package LeetcodePractice;
+
+import java.util.*;
+
+public class AnagramChecker {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++; 
+            count[t.charAt(i) - 'a']--; 
+        }
+
+        for (int c : count) {
+            if (c != 0) return false; 
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram")); 
+        System.out.println(isAnagram("rat", "car"));        
+    }
+}
+
