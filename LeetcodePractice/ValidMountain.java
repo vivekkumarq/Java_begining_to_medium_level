2f0b9a8c55b4cc4c1b5a8dd4bf0c19c99f7c96a7
vivek kumar <51886550+vivekkumarq@users.noreply.github.com>
2025-10-07 23:46:13 +0530

    ValidMountain

---
+++ LeetcodePractice/ValidMountain.java
@@ -0,0 +1,33 @@
+package LeetcodePractice;
+
+public class ValidMountain {
+     public boolean validMountainArray(int[] arr) {
+        int n = arr.length;
+        if (n < 3) return false;
+
+        int i = 0;
+
+       
+        while (i + 1 < n && arr[i] < arr[i + 1]) {
+            i++;
+        }
+
+        
+        if (i == 0 || i == n - 1) return false;
+
+       
+        while (i + 1 < n && arr[i] > arr[i + 1]) {
+            i++;
+        }
+
+        
+        return i == n - 1;
+    }
+
+    public static void main(String[] args) {
+        ValidMountain sol = new ValidMountain();
+        System.out.println(sol.validMountainArray(new int[]{2,1}));        
+        System.out.println(sol.validMountainArray(new int[]{3,5,5}));     
+        System.out.println(sol.validMountainArray(new int[]{0,3,2,1}));  
+    }
+}
